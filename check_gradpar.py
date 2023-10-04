991ac5ecbf8faa9b3c75ad37d32bc010d0f90bc9
daringli <nowaya@lol.com>
2023-10-03 20:39:09 -0400

    Added file to check gradpar

---
+++ check_gradpar.py
@@ -0,0 +1,14 @@
+from scipy.io import netcdf_file
+import numpy as np
+
+if __name__ == "__main__":
+    import sys
+    ncfile = sys.argv[1]
+
+
+    with netcdf_file(ncfile) as f:
+        gradpar = f.variables["gradpar"][()]
+
+
+    np.set_printoptions(32)
+    print(gradpar)
